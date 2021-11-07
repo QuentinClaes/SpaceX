d6a794de3cb6f475b3860be630f2192c22973474
Quentin Claes <quentin@cuustomer.com>
2021-11-07 12:41:57 +0100

    Update posts.js

--- src/components/posts.js
+++ src/components/posts.js
@@ -11,27 +11,16 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
 import Avatar from "@mui/material/Avatar";
-import TablePagination from "@mui/material/TablePagination";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Collapse from "@mui/material/Collapse";
 import YouTube from "react-youtube";
-import IconButton from "@mui/material/IconButton";
-import { red } from "@mui/material/colors";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import ShareIcon from "@mui/icons-material/Share";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
 import Pagination from "@mui/material/Pagination";
+import Alert from "@mui/material/Alert";
 
 const style = {
   position: "absolute",
@@ -46,8 +35,12 @@ const style = {
 };
 
 const WrapperContainer = styled.div`
-  padding-top: 200px;
+  padding-top: 100px;
   padding-bottom: 200px;
+`;
+
+const WrapperPaper = styled(Paper)`
+  padding: 20px;
 `;
 
 const Posts = () => {
@@ -81,16 +74,10 @@ const Posts = () => {
   }, [dispatch, limit, page]);
 
   const handleChangePage = (event, newPage) => {
-    console.log("mon event", event);
     setPage(newPage);
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setLimit(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  const handleOpen1 = (index) => {
+  const handleModalOpen = (index) => {
     setIndex(index);
     handleOpen();
   };
@@ -98,101 +85,142 @@ const Posts = () => {
   function isDateBeforeToday(date) {
     return new Date(date.toDateString()) < new Date(new Date().toDateString());
   }
-  console.log("mes ifos", posts);
+
   return (
-    <WrapperContainer>
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Patch</TableCell>
-              <TableCell align="left">Name</TableCell>
-              <TableCell align="left">Details</TableCell>
-              <TableCell align="left">Date</TableCell>
-              <TableCell align="center">Success</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {posts.docs && [0]
-              ? posts.docs.map((row, index) => (
-                  <>
-                    <TableRow
-                      key={row.id}
-                      onClick={() => handleOpen1(index)}
-                      sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                    >
-                      <TableCell align="left">
-                        <Avatar alt="Remy Sharp" src={row.links.patch.small} />
-                      </TableCell>
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="left">{row.details}</TableCell>
-                      <TableCell align="left">
-                        {new Date(row.date_utc).toLocaleDateString("en-US")}
-                      </TableCell>
-                      <TableCell align="center">
-                        {isDateBeforeToday(new Date(row.date_utc)) ? (
-                          <Chip
-                            label={row.success ? "Success" : "Failure"}
-                            color={row.success ? "success" : "error"}
+    <>
+      <Box
+        sx={{
+          paddingTop: 15,
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
+            m: 1,
+            width: "100%",
+          },
+        }}
+      >
+        <WrapperPaper elevation={3}>
+          <h2>👨🏽‍🚀 SpaceX REST API testing page 👨🏽‍🚀</h2>
+          <p>
+            Small page to test the SpaceX REST API. I choose to display all the
+            past launches in a table.You can click on every row of the table to
+            open a modal displaying the informations about the selected launch.
+          </p>
+          <Alert severity="warning">
+            We are not affiliated, associated, authorized, endorsed by, or in
+            any way officially connected with Space Exploration Technologies
+            Corp (SpaceX), or any of its subsidiaries or its affiliates. The
+            names SpaceX as well as related names, marks, emblems and images are
+            registered trademarks of their respective owners.
+          </Alert>
+        </WrapperPaper>
+      </Box>
+      <WrapperContainer>
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Patch</TableCell>
+                <TableCell align="left">Name</TableCell>
+                <TableCell align="left">Details</TableCell>
+                <TableCell align="left">Date</TableCell>
+                <TableCell align="center">Success</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {posts.docs && [0]
+                ? posts.docs.map((row, index) => (
+                    <>
+                      <TableRow
+                        key={row.id}
+                        onClick={() => handleModalOpen(index)}
+                        sx={{
+                          "&:last-child td, &:last-child th": { border: 0 },
+                        }}
+                      >
+                        <TableCell align="left">
+                          <Avatar
+                            alt="Remy Sharp"
+                            src={row.links.patch.small}
                           />
-                        ) : (
-                          <Chip
-                            label={"hasn't taken off yet"}
-                            color={"warning"}
-                          />
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  </>
-                ))
-              : null}
-          </TableBody>
-        </Table>
-        <Pagination
-          count={posts.totalPages}
-          page={page}
-          onChange={handleChangePage}
-        />
-      </TableContainer>
-      {posts.docs && [index] ? (
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="modal-modal-title"
-          aria-describedby="modal-modal-description"
-        >
-          <Card sx={style}>
-            <CardHeader
-              avatar={
-                <Avatar
-                  alt="SpaceX"
-                  src={posts.docs[index].links.patch.large}
-                  sx={{ width: 150, height: 150 }}
-                />
-              }
-              title={"🚀 " + posts.docs[index].name + " 🚀"}
-              subheader={
-                "📅" +
-                new Date(posts.docs[index].date_utc).toLocaleDateString("en-US")
-              }
-            />
-            {posts.docs[index].links.youtube_id ? (
-              <YouTube
-                videoId={posts.docs[index].links.youtube_id}
-                opts={youtubeOPTS}
+                        </TableCell>
+                        <TableCell component="th" scope="row">
+                          {row.name}
+                        </TableCell>
+                        <TableCell align="left">{row.details}</TableCell>
+                        <TableCell align="left">
+                          {new Date(row.date_utc).toLocaleDateString("fr-FR")}
+                        </TableCell>
+                        <TableCell align="center">
+                          {isDateBeforeToday(new Date(row.date_utc)) ? (
+                            <Chip
+                              label={row.success ? "Success" : "Failure"}
+                              color={row.success ? "success" : "error"}
+                            />
+                          ) : (
+                            <Chip
+                              label={"hasn't taken off yet"}
+                              color={"warning"}
+                            />
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    </>
+                  ))
+                : null}
+            </TableBody>
+          </Table>
+          <Pagination
+            count={posts.totalPages}
+            page={page}
+            onChange={handleChangePage}
+          />
+        </TableContainer>
+        {posts.docs && [index] ? (
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Card sx={style}>
+              <CardHeader
+                avatar={
+                  <Avatar
+                    alt="SpaceX"
+                    src={
+                      posts.docs[index].links.patch.large
+                        ? posts.docs[index].links.patch.large
+                        : null
+                    }
+                    sx={{ width: 100, height: 100 }}
+                  />
+                }
+                title={"🚀 " + posts.docs[index].name + " 🚀"}
+                subheader={
+                  "📅 " +
+                  new Date(posts.docs[index].date_utc).toLocaleDateString(
+                    "fr-FR"
+                  ) +
+                  " 📅"
+                }
               />
-            ) : null}
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                {posts.docs[index].details}
-              </Typography>
-            </CardContent>
-          </Card>
-        </Modal>
-      ) : null}
-    </WrapperContainer>
+              {posts.docs[index].links.youtube_id ? (
+                <YouTube
+                  videoId={posts.docs[index].links.youtube_id}
+                  opts={youtubeOPTS}
+                />
+              ) : null}
+              <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                  {posts.docs[index].details}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Modal>
+        ) : null}
+      </WrapperContainer>
+    </>
   );
 };
 
